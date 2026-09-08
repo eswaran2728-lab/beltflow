cf4e406441235e7981adbe81b9ffb1180e54aec2
Master Eswaran <eswaran2728@gmail.com>
2026-09-08 16:59:00 +0800

    build: bump versionCode to 14 and versionName to 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.eswaran.beltflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
