e756ddfbbc8b6d9068dd5369816b819eb7ef7a4e
Master Eswaran <eswaran2728@gmail.com>
2026-09-08 11:32:25 +0800

    build: bump targetSdk and compileSdk to 36 and bump versionCode to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 android {
     namespace = "com.example.beltflow"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.eswaran.beltflow"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.10"
+        targetSdk = 36
+        versionCode = 12
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
