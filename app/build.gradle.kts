37b050382506b01b9e19236e5762a1ad6c1c6749
eswaran2728-lab <eswaran2728@gmail.com>
2026-09-08 10:04:33 +0800

    chore: bump app version to 1.0.9
    Update version code to 10 and version name to 1.0.9 in build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.eswaran.beltflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
